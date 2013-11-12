abc82293267aa77d1cf197bed77f4c77e677b007
Joe Smith <yasumoto7@gmail.com>
2013-11-11 23:24:18 -0800

    Initial layout and creation of view controller

---
+++ WerkOut/BJViewController.m
@@ -0,0 +1,26 @@
+//
+//  BJViewController.m
+//  WerkOut
+//
+//  Created by Joseph Smith on 11/11/13.
+//  Copyright (c) 2013 Joseph Smith. All rights reserved.
+//
+
+#import "BJViewController.h"
+
+@interface BJViewController ()
+
+@end
+
+@implementation BJViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
