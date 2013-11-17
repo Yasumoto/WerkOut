d332e576a5500e246a45a9c675774b937dc04520
Joe Smith <yasumoto7@gmail.com>
2013-11-16 20:25:32 -0800

    werk data

---
+++ WerkOut/BJWerkData.h
@@ -0,0 +1,15 @@
+//
+//  BJWerkData.h
+//  WerkOut
+//
+//  Created by Joseph Smith on 11/16/13.
+//  Copyright (c) 2013 Joseph Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BJWerkData : NSObject
+@property (assign) int intensity;
+@property (assign) int crunches;
+@property (assign) int treadmill;
+@end
---
+++ WerkOut/BJWerkData.m
@@ -0,0 +1,13 @@
+//
+//  BJWerkData.m
+//  WerkOut
+//
+//  Created by Joseph Smith on 11/16/13.
+//  Copyright (c) 2013 Joseph Smith. All rights reserved.
+//
+
+#import "BJWerkData.h"
+
+@implementation BJWerkData
+
+@end
